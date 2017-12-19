9beed2e235292a8a37ac4f770ff5ee43f2a20800
Vladimir Shkerin <vladshkerin@gmail.com>
2017-12-19 07:55:24 +0300

    Add module 'dialog.js'

---
+++ js/dialog.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * Модуль для перетаскивания диалога редактирования персонажа
+ * и предметов из магазина в рюкзак.
+ */
+
+(function () {
+  var setup = document.querySelector('.setup');
+  var dialog = document.querySelector('.upload'); // TODO заменить на класс '.setup-user-pic'
+  var setupCloseElem = document.querySelector('.setup-close');
+  var saveCoords;
+
+  dialog.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    if (!saveCoords) {
+      saveCoords = {
+        left: setup.offsetLeft,
+        top: setup.offsetTop
+      };
+    }
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var mouseMoveHandler = function (moveEvt) {
+      moveEvt.preventDefault();
+      moveEvt.stopPropagation();
+
+      var shiftCoords = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.x,
+        y: moveEvt.y
+      };
+
+      setup.style.left = (setup.offsetLeft - shiftCoords.x) + 'px';
+      setup.style.top = (setup.offsetTop - shiftCoords.y) + 'px';
+    };
+
+    var mouseUpHandler = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+
+    var setupCloseHandler = function (closeEvt) {
+      closeEvt.preventDefault();
+
+      setup.style.left = saveCoords.left + 'px';
+      setup.style.top = saveCoords.top + 'px';
+    };
+    setupCloseElem.addEventListener('click', setupCloseHandler);
+  });
+})();
